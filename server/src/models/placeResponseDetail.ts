4ea0f29c18daaea4d5f28780ad7fc7934c93186a
jimwell <jimwelllmanalo@gmail.com>
2025-04-23 22:34:53 +0800

    Create interface for places API response

---
+++ server/src/models/placeResponseDetail.ts
@@ -0,0 +1,11 @@
+import { PlacePhoto } from "./placePhoto";
+
+export interface PlaceResponseDetail {
+  id: string;
+  formattedAddress: string;
+  googleMapsUri: string;
+  displayName: {
+    text: string;
+  };
+  photos: PlacePhoto[];
+}
