d06fa4e392c809599589e81fc32faa826e433001
jimwell <jimwelllmanalo@gmail.com>
2025-04-24 15:35:39 +0800

    Use validation middlewares to searchPlace and getPhoto routes

--- server/src/routes/placeRoutes.ts
+++ server/src/routes/placeRoutes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { getPlacePhoto, searchPlaces } from "../controllers/placeController";
+import { checkSchema } from "express-validator";
+import { queryValidationSchema } from "../utils/validationSchemas/querySchema";
+import { photoReferenceValidationSchema } from "../utils/validationSchemas/photoReferenceSchema";
 const router: Router = Router();
 
 //route to conduct a search for places and their details through a query
-router.post("/", searchPlaces);
+router.post("/", checkSchema(queryValidationSchema), searchPlaces);
 //proxy route to get place photo
-router.get("/photo", getPlacePhoto);
+router.get(
+  "/photo",
+  checkSchema(photoReferenceValidationSchema),
+  getPlacePhoto
+);
 
 export default router;
