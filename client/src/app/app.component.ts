657a80c7ed288cf8b76d22f7415417cbd573f948
jimwell <jimwelllmanalo@gmail.com>
2025-04-27 23:19:00 +0800

    Import place-list and map component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { PlaceSearchComponent } from './components/place-search/place-search.component';
+import { MapComponent } from './components/map/map.component';
+import { PlacesListComponent } from './components/places-list/places-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [PlaceSearchComponent],
+  imports: [PlaceSearchComponent, MapComponent, PlacesListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
