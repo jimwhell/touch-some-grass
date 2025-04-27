2c1bb75ea149b71136d80f359dc10aa8ef1ea7e3
jimwell <jimwelllmanalo@gmail.com>
2025-04-28 00:59:59 +0800

    Create method to format place type string

--- client/src/app/components/place-details/place-details.component.ts
+++ client/src/app/components/place-details/place-details.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, input, InputSignal } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
 import { Place } from '../../interfaces/place';
 import { QueryService } from '../../services/query.service';
 
 @Component({
   selector: 'app-place-details',
-  imports: [],
+  imports: [TitleCasePipe],
   templateUrl: './place-details.component.html',
   styleUrl: './place-details.component.css',
 })
@@ -15,5 +16,10 @@ export class PlaceDetailsComponent {
 
   getPhotoFromReference(photoReference: string) {
     return this.queryService.getPhoto(photoReference);
+  }
+
+  getFormattedType(): string {
+    const placeType: string = this.place()?.primaryType!;
+    return placeType.replaceAll('_', ' ');
   }
 }
