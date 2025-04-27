01ff0ea04ff9a2d38cf4ba5144fc239fa1baeb65
jimwell <jimwelllmanalo@gmail.com>
2025-04-27 23:22:11 +0800

    Create places subject and assign value to observables

--- client/src/app/services/query.service.ts
+++ client/src/app/services/query.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { PlaceQuery } from '../interfaces/place-query';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { Place } from '../interfaces/place';
 @Injectable({
   providedIn: 'root',
 })
 export class QueryService {
   constructor(private apiService: ApiService) {}
+  private placesSubject = new BehaviorSubject<Place[]>([]);
+  places$ = this.placesSubject.asObservable();
+
   private apiUrl: string = '/api/places';
 
   //method to fetch places from server
   searchPlaces(query: PlaceQuery): Observable<Place[]> {
-    return this.apiService.post(this.apiUrl, query);
+    return this.apiService.post<Place[], PlaceQuery>(this.apiUrl, query).pipe(
+      tap((places: Place[]) => {
+        this.placesSubject.next(places);
+        console.log(`places received: ${places}`);
+      })
+    );
   }
 
   //method to return the route endpoint for the template to fetch the image from the server
