332ec6b22d6dbcc302f8e8c9345d82618c7285a4
jimwell <jimwelllmanalo@gmail.com>
2025-04-28 15:42:02 +0800

    Refactor getformattedType method to return undefined or the formattedType string

--- client/src/app/components/place-details/place-details.component.ts
+++ client/src/app/components/place-details/place-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input, InputSignal } from '@angular/core';
+import { Component, Input, input, InputSignal, OnInit } from '@angular/core';
 import { TitleCasePipe } from '@angular/common';
 import { Place } from '../../interfaces/place';
 import { QueryService } from '../../services/query.service';
@@ -9,17 +9,26 @@ import { QueryService } from '../../services/query.service';
   templateUrl: './place-details.component.html',
   styleUrl: './place-details.component.css',
 })
-export class PlaceDetailsComponent {
-  constructor(private queryService: QueryService) {}
-
+export class PlaceDetailsComponent implements OnInit {
+  formattedType!: string | undefined;
   place: InputSignal<Place | undefined> = input<Place>();
+
+  ngOnInit(): void {
+    this.formattedType = this.getFormattedType();
+  }
+
+  constructor(private queryService: QueryService) {}
 
   getPhotoFromReference(photoReference: string) {
     return this.queryService.getPhoto(photoReference);
   }
 
-  getFormattedType(): string {
+  //if place object contains primary type attribute, return formatted type. Else, cancel the execution of the method.
+  getFormattedType(): string | undefined {
     const placeType: string = this.place()?.primaryType!;
+    if (!placeType) {
+      return;
+    }
     return placeType.replaceAll('_', ' ');
   }
 }
