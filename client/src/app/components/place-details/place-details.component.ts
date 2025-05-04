263d7f2cf7a3f142e30da672712852dcf8abf544
jimwell <jimwelllmanalo@gmail.com>
2025-05-04 15:23:34 +0800

    Omit place argument from emit method and create getPlaceDetails method

--- client/src/app/components/place-details/place-details.component.ts
+++ client/src/app/components/place-details/place-details.component.ts
@@ -9,6 +9,9 @@ import {
 import { TitleCasePipe } from '@angular/common';
 import { Place } from '../../interfaces/place';
 import { QueryService } from '../../services/query.service';
+import { PlaceDetail } from '../../interfaces/place-detail';
+import { Observable } from 'rxjs';
+import { ExpandedPlace } from '../../interfaces/expanded-place';
 
 @Component({
   selector: 'app-place-details',
@@ -19,22 +22,34 @@ import { QueryService } from '../../services/query.service';
 export class PlaceDetailsComponent implements OnInit {
   formattedType!: string | undefined;
   place: InputSignal<Place | undefined> = input<Place>();
-  placeCardClicked: OutputEmitterRef<Place> = output<Place>();
+  placeCardClicked: OutputEmitterRef<ExpandedPlace> = output<ExpandedPlace>();
+  placeDetails!: PlaceDetail;
 
   //on initialization, assigns the formatted type variable to the value returned by the getFormattedType method
   ngOnInit(): void {
     this.formattedType = this.getFormattedType();
+    this.getPhotoFromReference();
   }
 
   constructor(private queryService: QueryService) {}
 
-  getPhotoFromReference(photoReference: string) {
-    return this.queryService.getPhoto(photoReference);
+  //assign the file image request link as a property to the place object
+  getPhotoFromReference() {
+    const place = this.place();
+    const placePhoto = place?.photo;
+    if (placePhoto) {
+      const placePhotoFile = this.queryService.getPhoto(place?.photo);
+      place.photo = placePhotoFile;
+    }
   }
 
-  emitPlaceClick(event: MouseEvent, place: Place) {
-    console.log(place);
-    this.placeCardClicked.emit(place);
+  emitPlaceClick(event: MouseEvent, placeDetails: PlaceDetail) {
+    const expandedPlace: ExpandedPlace = {
+      ...this.place()!,
+      ...this.placeDetails,
+    };
+
+    this.placeCardClicked.emit(expandedPlace);
   }
 
   //if place object contains primary type attribute, return formatted type. Else, cancel the execution of the method.
@@ -44,5 +59,22 @@ export class PlaceDetailsComponent implements OnInit {
       return;
     }
     return placeType.replaceAll('_', ' ');
+  }
+
+  getPlaceDetails($event: MouseEvent) {
+    const placeId = this.place()?.id;
+    if (!placeId) {
+      console.error('Place ID not found.');
+      return;
+    }
+    this.queryService.getPlaceDetails(placeId).subscribe({
+      next: (response: PlaceDetail) => {
+        this.placeDetails = response;
+        this.emitPlaceClick($event, this.placeDetails);
+      },
+      error: (err) => {
+        console.error('Error in fecthing place details: ', err);
+      },
+    });
   }
 }
