7b385463ab96fb825fb5705b4c58498c39ab2fd6
jimwell <jimwelllmanalo@gmail.com>
2025-05-06 20:28:14 +0800

    Create map place card component and display data on template

--- client/src/app/components/map-place-card/map-place-card.component.ts
+++ client/src/app/components/map-place-card/map-place-card.component.ts
@@ -1,13 +1,32 @@
-import { Component, input, InputSignal } from '@angular/core';
+import {
+  Component,
+  input,
+  InputSignal,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { ExpandedPlace } from '../../interfaces/expanded-place';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { PlaceService } from '../../services/place.service';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-map-place-card',
-  imports: [],
+  imports: [RouterOutlet, RouterLink, TitleCasePipe],
   templateUrl: './map-place-card.component.html',
   styleUrl: './map-place-card.component.css',
 })
-export class MapPlaceCardComponent {
+export class MapPlaceCardComponent implements OnChanges {
   selectedPlace: InputSignal<ExpandedPlace | undefined> =
     input<ExpandedPlace>();
+
+  constructor(private placeService: PlaceService) {}
+
+  //if selectedPlace input property gains a value, assign its value to the selectedPlace subject inside placeService
+  ngOnChanges(changes: SimpleChanges): void {
+    const selectedPlaceData: ExpandedPlace | undefined = this.selectedPlace();
+    if (selectedPlaceData != undefined) {
+      this.placeService.assignSelectedPlace(selectedPlaceData);
+    }
+  }
 }
