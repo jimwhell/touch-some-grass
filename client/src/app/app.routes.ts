8f6cebe7e3f80b95bd6051e6b9ad041d026d569d
jimwell <jimwelllmanalo@gmail.com>
2025-05-06 20:27:05 +0800

    Create routes for overview and reviews component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PlaceOverviewComponent } from './components/place-overview/place-overview.component';
+import { PlaceReviewsComponent } from './components/place-reviews/place-reviews.component';
+import { MapPlaceCardComponent } from './components/map-place-card/map-place-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'overview',
+    component: PlaceOverviewComponent,
+  },
+  {
+    path: 'reviews',
+    component: PlaceReviewsComponent,
+  },
+];
