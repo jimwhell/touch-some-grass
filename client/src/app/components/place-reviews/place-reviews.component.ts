c101f315c06a7007c5e3d193dec04432d3568be5
jimwell <jimwelllmanalo@gmail.com>
2025-05-06 20:42:13 +0800

    Create place reviews component

---
+++ client/src/app/components/place-reviews/place-reviews.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-place-reviews',
+  imports: [],
+  templateUrl: './place-reviews.component.html',
+  styleUrl: './place-reviews.component.css'
+})
+export class PlaceReviewsComponent {
+
+}
