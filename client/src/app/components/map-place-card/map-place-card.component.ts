3ca5ea8c82d16a1b4bf0efba24655fcd1df8aba4
jimwell <jimwelllmanalo@gmail.com>
2025-05-07 15:23:01 +0800

    Redirect body section to overview component upon initialization

--- client/src/app/components/map-place-card/map-place-card.component.ts
+++ client/src/app/components/map-place-card/map-place-card.component.ts
@@ -4,9 +4,15 @@ import {
   InputSignal,
   OnChanges,
   SimpleChanges,
+  OnInit,
 } from '@angular/core';
 import { ExpandedPlace } from '../../interfaces/expanded-place';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import {
+  RouterOutlet,
+  RouterLink,
+  RouterLinkActive,
+  Router,
+} from '@angular/router';
 import { PlaceService } from '../../services/place.service';
 import { TitleCasePipe } from '@angular/common';
 
@@ -16,11 +22,11 @@ import { TitleCasePipe } from '@angular/common';
   templateUrl: './map-place-card.component.html',
   styleUrl: './map-place-card.component.css',
 })
-export class MapPlaceCardComponent implements OnChanges {
+export class MapPlaceCardComponent implements OnInit, OnChanges {
   selectedPlace: InputSignal<ExpandedPlace | undefined> =
     input<ExpandedPlace>();
 
-  constructor(private placeService: PlaceService) {}
+  constructor(private placeService: PlaceService, private router: Router) {}
 
   //if selectedPlace input property gains a value, assign its value to the selectedPlace subject inside placeService
   ngOnChanges(changes: SimpleChanges): void {
@@ -28,5 +34,10 @@ export class MapPlaceCardComponent implements OnChanges {
     if (selectedPlaceData != undefined) {
       this.placeService.assignSelectedPlace(selectedPlaceData);
     }
+  }
+
+  //Upon initialization, display overview component by default.
+  ngOnInit(): void {
+    this.router.navigate(['/overview']);
   }
 }
