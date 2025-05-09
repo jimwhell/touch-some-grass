11c114a04923a7ad984a7957a78bcbbf89e7cc98
jimwell <jimwelllmanalo@gmail.com>
2025-05-09 14:37:01 +0800

    Import ngx stars

--- client/src/app/components/map-place-card/map-place-card.component.ts
+++ client/src/app/components/map-place-card/map-place-card.component.ts
@@ -14,29 +14,47 @@ import {
   Router,
 } from '@angular/router';
 import { PlaceService } from '../../services/place.service';
-import { TitleCasePipe } from '@angular/common';
+import { TitleCasePipe, CommonModule } from '@angular/common';
+import { Observable, filter } from 'rxjs';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { NgxStarsModule } from 'ngx-stars';
 
 @Component({
   selector: 'app-map-place-card',
-  imports: [RouterOutlet, RouterLink, TitleCasePipe],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    TitleCasePipe,
+    CommonModule,
+    NgxStarsModule,
+  ],
   templateUrl: './map-place-card.component.html',
   styleUrl: './map-place-card.component.css',
 })
 export class MapPlaceCardComponent implements OnInit, OnChanges {
-  selectedPlace: InputSignal<ExpandedPlace | undefined> =
-    input<ExpandedPlace>();
+  //input signal consisting of the selectedPlace to be passed by the parent mapComponent
+  selectedPlace: InputSignal<ExpandedPlace | undefined> = input<
+    ExpandedPlace | undefined
+  >(undefined);
 
-  constructor(private placeService: PlaceService, private router: Router) {}
+  selectedPlace$!: Observable<ExpandedPlace>; //observable to hold the selectedPlace data, to be used in the template
 
-  //if selectedPlace input property gains a value, assign its value to the selectedPlace subject inside placeService
+  constructor(private placeService: PlaceService, private router: Router) {
+    // Convert the input signal to an observable and filter out undefined values for type narrowing, then assign the data to the selected place observable
+    this.selectedPlace$ = toObservable(this.selectedPlace).pipe(
+      filter((place): place is ExpandedPlace => place !== undefined)
+    );
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
+    //pass the place input property once it is loaded into the assignSelectedPlace method of placeService
     const selectedPlaceData: ExpandedPlace | undefined = this.selectedPlace();
-    if (selectedPlaceData != undefined) {
+    if (selectedPlaceData !== undefined) {
       this.placeService.assignSelectedPlace(selectedPlaceData);
     }
   }
 
-  //Upon initialization, display overview component by default.
+  //make /overview the default view upon component initialization
   ngOnInit(): void {
     this.router.navigate(['/overview']);
   }
